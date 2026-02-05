3a8957855e138b3339b58b718c1a54ae7dfeb85b
patricktsullivan <93329749+patricktsullivan@users.noreply.github.com>
2026-02-04 20:37:46 -0500

    Delete edr/crowdstrike/arbitrary_queries directory

--- edr/crowdstrike/arbitrary_queries/src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-NG-SIEM Hunter - Multi-tenant CrowdStrike NG-SIEM query tool.
-
-A CLI application for running arbitrary NG-SIEM queries across multiple
-CrowdStrike environments for incident response and threat hunting.
-"""
-
-__version__ = "0.1.0"
